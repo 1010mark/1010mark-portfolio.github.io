d258c511e1c3e34ae2b1de2edd1d9a71d1902816
1010mark <markmentaro@yahoo.co.jp>
2024-05-22 16:03:13 +0900

    adjust header and footer

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,8 +1,11 @@
 export default function Footer() {
     return (
         <footer className="bg-gray-100 p-2 text-gray-300 text-xs text-center">
-            Copyright © 2024 1010mark. All Rights Reserved.<br />
-            Footer ni nanikakeba iinoka wakaran.
+            <a href="https://x.com/3_Jugem">
+                <img src="./image/X_icon.png" width={18} className="opacity-50 my-2 mx-auto hover:opacity-90"/>
+            </a>
+            <p>Copyright © 2024 1010mark. All Rights Reserved.<br />
+            Footer ni nanikakeba iinoka wakaran.</p>
         </footer>
         )
 }
--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,12 +1,15 @@
+import Image from 'next/image'
 export default function Header() {
     return (
         <header className="text-gray-700 bg-gray-100 flex px-4 h-10 md:px-24 py-2">
             <div className="md:ml-10 ml-4 float-left flex">
-            <img src="./image/icon.png" className="h-full px-1" />
-            <p className="px-1">1010mark</p>
+                <Image src="./image/icon.png" height={24} width={20.47} alt="icon.png" />
+                <p className="px-1">1010mark</p>
             </div>
             <div className="md:mr-10 mr-4 ml-auto flex">
-                <p className="px-1 h-full">Top</p>
+                <a className="px-2 mx-1 h-full hover:border border-blue-600 hover:border-l-0 hover:border-t-0 hover:border-r-0" href="/">Top</a>
+                <a className="px-2 mx-1 h-full hover:border border-blue-600 hover:border-l-0 hover:border-t-0 hover:border-r-0" href="/profile">About</a>
+                <a className="px-2 mx-1 h-full hover:border border-blue-600 hover:border-l-0 hover:border-t-0 hover:border-r-0" href="/works">Works</a>
             </div>
         </header>
         )
