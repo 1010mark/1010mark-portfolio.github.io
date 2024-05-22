ffea34f58083903a13c52e8e1254412d9c8b9a5d
1010mark <markmentaro@yahoo.co.jp>
2024-05-22 13:30:47 +0900

    adjust nextjs presets for portfolio

---
+++ src/app/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer className="bg-gray-100 p-2 text-gray-300 text-xs text-center">
+            Copyright © 2024 1010mark. All Rights Reserved.<br />
+            Footer ni nanikakeba iinoka wakaran.
+        </footer>
+        )
+}
---
+++ src/app/header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <header className="bg-gray-100 flex px-4 h-10 md:px-24 py-2">
+            <div className="md:ml-10 ml-4 float-left flex">
+            <img src="/image/icon.png" className="h-full px-1" />
+            <p className="px-1">1010mark</p>
+            </div>
+            <div className="md:mr-10 mr-4 ml-auto flex">
+                <p className="px-1 h-full">Top</p>
+            </div>
+        </header>
+        )
+}
