0316c4574219274a1c7bcf7e1430bc137747ebf9
1010mark <markmentaro@yahoo.co.jp>
2024-05-22 15:35:20 +0900

    create toppage

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className="bg-gray-100 flex px-4 h-10 md:px-24 py-2">
+        <header className="text-gray-700 bg-gray-100 flex px-4 h-10 md:px-24 py-2">
             <div className="md:ml-10 ml-4 float-left flex">
             <img src="./image/icon.png" className="h-full px-1" />
             <p className="px-1">1010mark</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 export default function Home() {
   return (
-    <p>TopPage</p>
+    <main>
+      <div className="border-gray-400 text-center rounded-3xl border-gray-500 border font-light p-4">
+        <div>
+          <h1 className="m-4 text-3xl font-normal">脳内想像→具現人間、です。</h1>
+          <p>陽の光よりブルーライトを浴びて育ってきました。<br/>
+          「楽しそう！」をモットーに、常時何かを作っています。<br/>
+          とにかくなんでもさせてください。</p>
+        </div>
+        
+      </div>
+      <div className="flex flex-wrap justify-center m-2">
+        <a href="/profile" className="w-4/12 p-6 shadow-md text-center border-blue-600 border rounded-2xl m-2 font-normal hover:font-semibold hover:bg-indigo-700 hover:text-white">About</a>
+        <a href="/works" className="w-4/12 p-6 shadow-md text-center border-blue-600 border rounded-2xl m-2 font-normal hover:font-semibold hover:bg-indigo-700 hover:text-white">Works</a>
+      </div>
+    </main>
   );
 }
