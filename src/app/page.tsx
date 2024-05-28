d07cd21719df51100a5c458fd8603741d9051765
1010mark <markmentaro@yahoo.co.jp>
2024-05-28 13:45:07 +0900

    some fix in top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         before:absolute before:content-[''] before:left-[-20vw] before:top-0 before:bottom-0 before:z-[-1] before:w-[120vw] before:bg-black
         after:absolute after:content-[''] after:top-0 after:bottom-0 after:z-10 after:w-full after:[background:linear-gradient(to_right,black_0%,transparent_5%,transparent_95%,black_100%)]">
         <h1 className="[writing-mode:vertical-rl] text-white absolute text-5xl font-medium top-[30%] md:right-[30%] left-[20%]">妄想駆動。</h1>
-      </div>}}
+      </div>}
       <div className="border-gray-400 text-center rounded-3xl border-gray-500 border font-light p-4">
         <div>
           <h1 className="m-4 text-3xl font-normal">ワクワクあふれる作品制作。</h1>
