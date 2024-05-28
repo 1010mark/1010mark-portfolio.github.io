f19a18b58e713147dc7d831b16928caf696eed40
1010mark <markmentaro@yahoo.co.jp>
2024-05-28 16:01:56 +0900

    small fix top in mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
       </div> 
       : <div className="mb-2 bg-cover md:[background-image:url(/1010mark-portfolio.github.io/image/works/shadowplay/shadowplay.png)] [background-image:url(/1010mark-portfolio.github.io/image/works/shadowplay/shadowplay_trim.png)] p-0 h-screen relative
         before:absolute before:content-[''] before:left-[-20vw] before:top-0 before:bottom-0 before:z-[-1] before:w-[120vw] before:bg-black
-        after:absolute after:content-[''] after:top-0 after:bottom-0 after:z-10 after:w-full after:[background:linear-gradient(to_right,black_0%,transparent_5%,transparent_95%,black_100%)]">
+        after:absolute after:content-[''] after:top-0 after:bottom-0 after:z-10 after:w-full after:md:[background:linear-gradient(to_right,black_0%,transparent_5%,transparent_95%,black_100%)]">
         <h1 className="[writing-mode:vertical-rl] text-white absolute text-5xl font-medium top-[30%] md:right-[30%] left-[20%]">妄想駆動。</h1>
       </div>}
       <div className="border-gray-400 text-center rounded-3xl border-gray-500 border font-light p-4">
