3ca08bee935c7f85ab79f2e19061ee6748e13740
markmentaro@yahoo.co.jp <markmentaro@yahoo.co.jp>
2024-06-09 23:39:38 +0900

    adjust position of 妄想駆動。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,12 +24,12 @@ export default function Home() {
       <div className="mb-2 bg-cover sm:[background-image:url(/image/works/shadowplay/shadowplay.png)] [background-image:url(/image/works/shadowplay/shadowplay_trim.png)] p-0 h-screen relative
         before:absolute before:content-[''] before:overflow-x-hidden before:left-[-20vw] before:p-0 before:mx-0 before:top-0 before:bottom-0 before:z-[-1] before:w-[120vw] before:bg-black
         after:absolute after:content-[''] after:top-0 after:bottom-0 after:z-10 after:w-full after:[background:linear-gradient(to_right,black_0%,transparent_5%,transparent_95%,black_100%)]">
-        <h1 className="[filter:drop-shadow(2px_2px_0_black)] [writing-mode:vertical-rl] text-white absolute text-5xl font-medium sm:top-[30%] top-[35%] left-[20%] sm:right-[30%] sm:left-[20%]">妄想駆動。</h1>
+        <h1 className="[filter:drop-shadow(2px_2px_0_black)] [writing-mode:vertical-rl] text-white absolute text-5xl font-medium sm:top-[30%] top-[35%] left-[20%] xl:right-[30%] md:left-[20%]">妄想駆動。</h1>
       </div> 
       : <div className="mb-2 bg-cover md:[background-image:url(/1010mark-portfolio.github.io/image/works/shadowplay/shadowplay.png)] [background-image:url(/1010mark-portfolio.github.io/image/works/shadowplay/shadowplay_trim.png)] p-0 h-screen relative
         before:absolute before:content-[''] before:left-[-20vw] before:top-0 before:bottom-0 before:z-[-1] before:w-[120vw] before:bg-black
         after:absolute after:content-[''] after:top-0 after:bottom-0 after:z-10 after:w-full after:[background:linear-gradient(to_right,black_0%,transparent_5%,transparent_95%,black_100%)]">
-        <h1 className="[writing-mode:vertical-rl] text-white absolute text-5xl font-medium top-[30%] md:right-[30%] left-[20%]">妄想駆動。</h1>
+        <h1 className="[filter:drop-shadow(2px_2px_0_black)] [writing-mode:vertical-rl] text-white absolute text-5xl font-medium sm:top-[30%] top-[35%] left-[20%] xl:right-[30%] md:left-[20%]">妄想駆動。</h1>
       </div>}
       <div className="border-gray-400 text-center rounded-3xl border-gray-500 border p-4">
         <div>
